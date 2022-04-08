743a4ae0f8a5bcfd930251e95b527da9c5bb06e0
rlichfield <92386608+rlichfield@users.noreply.github.com>
2022-04-08 08:26:46 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    number = randint(0, 1)
+    if (number == 1) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    arrow = randint(0, 3)
+    if (arrow == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (arrow == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    } else if (arrow == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    }
+})
+let arrow = 0
+let number = 0
+basic.showString("ASK A QUESTION")
 basic.forever(function () {
 	
 })
